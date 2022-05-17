fa448ab60ce2c67a79e81091b7903f6c09f34766
BALLNAZZAR-PC\Ballnazzar <ludek.p@email.cz>
2022-05-17 22:57:06 +0200

    dependent assertions

---
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sfgpetclinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerTest {
+
+    @Test
+    void dependentAssertions() {
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        owner.setCity("Key West");
+        owner.setTelephone("1212313123");
+
+        assertAll("Properties Test",
+                () -> assertAll("Person properties",
+                        () -> assertEquals("Joe", owner.getFirstName(), "First name did not match"),
+                        () -> assertEquals("Buck", owner.getLastName())),
+                () -> assertAll("Owner properties",
+                        () -> assertEquals("Key West", owner.getCity(), "City did not match"),
+                        () -> assertEquals("1212313123", owner.getTelephone()))
+                );
+    }
+}
