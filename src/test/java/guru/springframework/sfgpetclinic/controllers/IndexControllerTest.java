456cbcf56288cf01ab9baf4e5e4a7031d143e32a
BALLNAZZAR-PC\Ballnazzar <ludek.p@email.cz>
2022-05-18 22:14:13 +0200

    Add exceptions tests

--- src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/IndexControllerTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,19 +15,19 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("Test Proper View name is returned for index page")
     @Test
     void index() {
         assertEquals("index", controller.index());
         assertEquals("index", controller.index(), "Wrong View Returned");
 
-        assertEquals("index", controller.index(), () -> "Another Expensive Message " +
+        assertEquals("index", controller.index(), "Another Expensive Message " +
                 "Make me only if you have to");
     }
 
     @Test
+    @DisplayName("Test exception")
     void oupsHandler() {
-        assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive " +
-                "Message to build" +
-                "for my test");
+        assertThrows(ValueNotFoundException.class, () -> controller.ooupsHandler());
     }
 }
