a725a526cda12a7a675e68cad76eacdac2793be4
BALLNAZZAR-PC\Ballnazzar <ludek.p@email.cz>
2022-05-23 21:25:29 +0200

    Parametrized tests - csv source

--- src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/OwnerTest.java
@@ -1,7 +1,12 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -26,5 +31,30 @@ class OwnerTest implements ModelTests {
                 );
 
         assertThat(owner.getCity(), is("Key West"));
+    }
+
+    @DisplayName("Value Source Test - ")
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
+    @ValueSource(strings = {"Spring", "Framework", "Guru"})
+    void testValueSource(String value) {
+        System.out.println(value);
+    }
+
+    @DisplayName("Enum Source Test - ")
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
+    @EnumSource(OwnerType.class)
+    void enumTest(OwnerType ownerType) {
+        System.out.println(ownerType);
+    }
+
+    @DisplayName("CSV Input Test - ")
+    @ParameterizedTest(name = "{displayName} [{index}] {argumentsWithNames}")
+    @CsvSource({
+            "FL,1,1",
+            "OH,2,2",
+            "MI,3,2"
+    })
+    void csvInputTest(String state_name, int val1, int val2) {
+        System.out.println(state_name + " = " + val1 + ":" + val2);
     }
 }
