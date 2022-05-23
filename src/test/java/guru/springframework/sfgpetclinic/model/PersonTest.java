ef7aff76c21bfce98de89b7e19854b60212d6eba
BALLNAZZAR-PC\Ballnazzar <ludek.p@email.cz>
2022-05-23 20:23:15 +0200

    rpeated tests interface

--- src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/model/PersonTest.java
@@ -1,9 +1,7 @@
 package guru.springframework.sfgpetclinic.model;
 
 import guru.springframework.sfgpetclinic.ModelTests;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,13 +25,6 @@ class PersonTest implements ModelTests {
         assertAll("Test Props Set",
                 () -> assertEquals("Joe", person.getFirstName(), "First Name Failed"),
                 () -> assertEquals("Buck", person.getLastName(), "Last Name Failed"));
-    }
-
-    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} of {totalRepetitions}")
-    @DisplayName("My Repeated Test")
-    @Test
-    void myRepeatedTests() {
-        // TODO
     }
 
 }
